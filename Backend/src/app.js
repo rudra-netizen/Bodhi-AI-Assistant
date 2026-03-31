0869a27db63556e37cdcd8af41ba08176c2f15af
rudra-netizen <rudrakshtiwari972@gmail.com>
2026-03-31 20:16:11 +0530

    Fixed Route

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,12 +5,16 @@ const chatRoutes = require("./routes/chat.routes");
 const cors = require("cors");
 
 const app = express();
+app.get("/", (req, res) => {
+  res.send("Bodhi AI Assistant is running 🚀");
+});
 app.use(
   cors({
     origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true,
   }),
 );
+
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
