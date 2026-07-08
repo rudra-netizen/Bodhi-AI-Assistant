a38fa9d5f28712203af80dd4004c244e6a5e820a
rudra-netizen <rudrakshtiwari972@gmail.com>
2026-07-08 15:41:31 +0530

    Image understanding feature integrated

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -30,7 +30,8 @@ app.use(
   }),
 );
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "../public")));
 app.use("/api/auth", authRoutes);
